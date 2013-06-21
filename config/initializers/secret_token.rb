25c676ba60feca37f7816fad39e0f71e6eb3e81a
captconrado <ottey001@gmail.com>
2013-06-20 20:01:58 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwipeBook::Application.config.secret_token = 'fe3e19db66b019249efa8f33f2b879f062d2585d303a3fff55112d423972a26d3feac0944ab3666703c64e100954fb89261909b3fbadbd1f4ff27afe4e3605b0'
